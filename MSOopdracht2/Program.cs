c70fca139345ae1418c6e29a04513b3293754bf7
Alicia Torre van Schouwen <a.f.torrevanschouwen@students.uu.nl>
2025-10-07 15:16:10 +0200

    psuh

---
+++ MSOopdracht2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MSOopdracht2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
