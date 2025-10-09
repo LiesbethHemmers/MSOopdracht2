356970a10cf20ff6b0cad656d1cb611013f8e892
Alicia Torre van Schouwen <a.f.torrevanschouwen@students.uu.nl>
2025-10-09 12:39:03 +0200

    Added a file, that can be used to test if the parser works

--- MSOopdracht2/obj/Debug/net6.0/MSOopdracht2.AssemblyInfo.cs
+++ MSOopdracht2/obj/Debug/net6.0/MSOopdracht2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MSOopdracht2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2ea11374449c37f646ddc42e096ce19934da70e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6011ca3709f6803445e882f0d33d4824e763711")]
 [assembly: System.Reflection.AssemblyProductAttribute("MSOopdracht2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MSOopdracht2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
