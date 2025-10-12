23fe3d5dc04c6f71206c4d1bade06400666074d2
liesb <liesb@LIESBETH>
2025-10-12 18:16:32 +0200

    everything works

--- MSOopdracht2/obj/Debug/net6.0/MSOopdracht2.AssemblyInfo.cs
+++ MSOopdracht2/obj/Debug/net6.0/MSOopdracht2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MSOopdracht2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac0272c6a6fd7dee1f5a625a183fc80c4d6d60a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0e6bed3a127e84ca877bc918cb91efa94750502")]
 [assembly: System.Reflection.AssemblyProductAttribute("MSOopdracht2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MSOopdracht2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
