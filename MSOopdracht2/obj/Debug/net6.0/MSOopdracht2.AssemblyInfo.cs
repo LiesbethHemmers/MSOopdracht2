f84ff5d22ca17390b3363d71026dc44bfcbdd75d
Alicia Torre van Schouwen <a.f.torrevanschouwen@students.uu.nl>
2025-10-25 17:57:16 +0200

    put files in separate maps

--- MSOopdracht2/obj/Debug/net6.0/MSOopdracht2.AssemblyInfo.cs
+++ MSOopdracht2/obj/Debug/net6.0/MSOopdracht2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MSOopdracht2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cae53fb2a9521a173f3bfbfb0f0a0811ab588e22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c54560d4b2704a1955a5e65726b0a05037ebc9fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MSOopdracht2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MSOopdracht2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
