e8a8732175797664197256a9a8806c8df728990b
liesb <liesb@LIESBETH>
2025-10-26 14:36:08 +0100

    Put the progams without grids in files. I have to look how to make the code nicer so there's  less duplicated code.

--- MSOopdracht2/ExamplePrograms.cs
+++ MSOopdracht2/ExamplePrograms.cs
@@ -19,94 +19,87 @@ namespace MSOopdracht2
 
         static CodeProgram BasicProgram2()
         {
-            return new CodeProgram(new List<ICommand>
-            {
-                new MoveCommand(10),
-                new TurnCommand(TurnDirection.Right),
-                new MoveCommand(10),
-                new TurnCommand(TurnDirection.Right),
-                new MoveCommand(10),
-                new TurnCommand(TurnDirection.Right),
-                new MoveCommand(10),
-                new TurnCommand(TurnDirection.Right)
-                //The endpoint is (0,0) facing east
-            }, "BasicProgram2");
+            TxtProgramParser parser = new TxtProgramParser();
+            TxtProgramImporter import = new TxtProgramImporter(parser);
+            FileInfo fi = new FileInfo(@"Programs\basicProgram2.txt");
+            string fullFileName = fi.FullName;
+
+            CodeProgram basicProgram2 = import.Import(fullFileName);
+            basicProgram2.Name = "BasicProgram2";
+
+            return basicProgram2;
+            //The endpoint is (0,0) facing east
         }
 
         static CodeProgram AdvancedProgram1()
         {
-            return new CodeProgram(new List<ICommand>
-            {
-                new RepeatCommand(4, new List<ICommand>{new MoveCommand(10), new TurnCommand(TurnDirection.Right)})
-                //The endpoint is (0,0) facing east
-            }, "AdvancedProgram1");
+            TxtProgramParser parser = new TxtProgramParser();
+            TxtProgramImporter import = new TxtProgramImporter(parser);
+            FileInfo fi = new FileInfo(@"Programs\advancedProgram1.txt");
+            string fullFileName = fi.FullName;
+
+            CodeProgram advancedProgram1 = import.Import(fullFileName);
+            advancedProgram1.Name = "AdvancedProgram1";
+
+            return advancedProgram1;
+            //The endpoint is (0,0) facing east
         }
 
         static CodeProgram AdvancedProgram2()
         {
-            return new CodeProgram(new List<ICommand>
-            {
-                new RepeatCommand(3, new List<ICommand>{
-                new MoveCommand(2),
-                new TurnCommand(TurnDirection.Right),
-                new MoveCommand(3),
-                new TurnCommand(TurnDirection.Left),
-                new MoveCommand(4)}),
-                new TurnCommand(TurnDirection.Right),
-                new MoveCommand(1)
-                //The endpoint is (18,10) facing south
-            }, "AdvancedProgram2");
+            TxtProgramParser parser = new TxtProgramParser();
+            TxtProgramImporter import = new TxtProgramImporter(parser);
+            FileInfo fi = new FileInfo(@"Programs\advancedProgram2.txt");
+            string fullFileName = fi.FullName;
+
+            CodeProgram advancedProgram2 = import.Import(fullFileName);
+            advancedProgram2.Name = "AdvancedProgram2";
+
+            return advancedProgram2;
+            //The endpoint is (18,10) facing south
         }
 
         static CodeProgram ExpertProgram1()
         {
-            return new CodeProgram(new List<ICommand>
-            {
-                new MoveCommand(5),
-                new TurnCommand(TurnDirection.Left),
-                new TurnCommand(TurnDirection.Left),
-                new MoveCommand(3),
-                new TurnCommand(TurnDirection.Right),
-                new MoveCommand(4),
-                new RepeatCommand(3, new List<ICommand>{
-                new MoveCommand(1),
-                new TurnCommand(TurnDirection.Right),
-                new RepeatCommand(5, new List<ICommand>
-                {
-                    new MoveCommand(2)
-                })}),
-                new TurnCommand(TurnDirection.Left)
-                //The endpoint (3,6) facing south
-            }, "ExpertProgram1");
+            TxtProgramParser parser = new TxtProgramParser();
+            TxtProgramImporter import = new TxtProgramImporter(parser);
+            FileInfo fi = new FileInfo(@"Programs\expertProgram1.txt");
+            string fullFileName = fi.FullName;
+
+            CodeProgram expertProgram1 = import.Import(fullFileName);
+            expertProgram1.Name = "EpertProgram1";
+
+            return expertProgram1;
+            //The endpoint (3,6) facing south
         }
 
         static CodeProgram ExpertProgram2()
         {
-            return new CodeProgram(new List<ICommand>
-            {
-                new RepeatCommand(2, new List<ICommand>
-                {
-                    new MoveCommand(2),
-                    new RepeatCommand(3, new List<ICommand>
-                    {
-                        new MoveCommand(3),
-                        new RepeatCommand(2, new List<ICommand>{
-                            new TurnCommand(TurnDirection.Right)
-                        })
-                    })
-                })
-                //The endpoint (0,0) facing east
-            }, "ExpertProgram2");
+            TxtProgramParser parser = new TxtProgramParser();
+            TxtProgramImporter import = new TxtProgramImporter(parser);
+            FileInfo fi = new FileInfo(@"Programs\expertProgram2.txt");
+            string fullFileName = fi.FullName;
+
+            CodeProgram expertProgram2 = import.Import(fullFileName);
+            expertProgram2.Name = "EpertProgram2";
+
+            return expertProgram2;
+            //The endpoint (0,0) facing east
         }
 
         public static CodeProgram GetRandomExampleProgram()
         {
             Random random = new Random();
-            int choice = random.Next(1, 2);
+            int choice = random.Next(1, 7);
 
             return choice switch
             {
                 1 => BasicProgram1(),
+                2 => BasicProgram2(),
+                3 => AdvancedProgram1(),
+                4 => AdvancedProgram2(),
+                5 => ExpertProgram1(),
+                6 => ExpertProgram2(),
                 _ => throw new NotImplementedException() //Otherwise you will have a warning
             };
         }
--- MSOopdracht2/obj/Debug/net6.0/MSOopdracht2.AssemblyInfo.cs
+++ MSOopdracht2/obj/Debug/net6.0/MSOopdracht2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MSOopdracht2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a216963af793653a583b9eec40c9eb9dbcc30ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6a42600b097ace916eacbfd9616786da08d40f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MSOopdracht2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MSOopdracht2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
