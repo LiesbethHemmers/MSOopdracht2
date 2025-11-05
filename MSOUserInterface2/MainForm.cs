aa1a7a24d48da3620b11218ae14ec35b41a869e4
liesb <liesb@LIESBETH>
2025-11-05 15:23:51 +0100

    merge

---
+++ MSOUserInterface2/MainForm.cs
@@ -0,0 +1,198 @@
+using Microsoft.VisualBasic.ApplicationServices;
+using MSOopdracht2;
+using MSOopdracht2.Importers;
+using MSOopdracht2.Metrics;
+using MSOopdracht2.Parsers;
+using MSOopdracht2.Commands;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualStudio.TestPlatform.Common.Interfaces;
+using System.Diagnostics;
+
+namespace MSOUserInterface2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            _programPanel.BringToFront();
+        }
+
+        private void MetricsButtonClick(object sender, EventArgs e)
+        {
+            CodeProgram codeProgram = TextToCodeProgram();
+            MetricCalculator calculator = new MetricCalculator();
+            StoredMetrics metric = calculator.CalculateMetrics(codeProgram);
+            List<string> output = metric.GetMetrics();
+
+
+            _outputTextBox.Text = string.Join(" ", output);
+        }
+
+        private void RunButtonClick(object sender, EventArgs e)
+        {
+            CodeProgram codeProgram = TextToCodeProgram();
+            CodeProgramExecutor executor = new CodeProgramExecutor();
+            List<string> output = executor.Run(codeProgram);
+
+
+            _outputTextBox.Text = string.Join(" ", output);
+        }
+
+        private void FileLoadButtonClick(object sender, EventArgs e)
+        {
+            using OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                _programRichTextBox.Text = File.ReadAllText(filePath);
+                string fileAsText = _programRichTextBox.Text;
+            }
+        }
+
+        private void ToProgramButtonClick(object sender, EventArgs e)
+        {
+            _programPanel.BringToFront();
+        }
+        private void ToPathfindingButtonClick(object sender, EventArgs e)
+        {
+            _pathfindingPanel.BringToFront();
+        }
+
+        private void ExamplesComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (_examplesComboBox.Text.Equals("Basic"))
+            {
+                BasicClick();
+            }
+
+            if (_examplesComboBox.Text.Equals("Advanced"))
+            {
+                AdvancedClick();
+            }
+
+            if (_examplesComboBox.Text.Equals("Expert"))
+            {
+                ExpertClick();
+            }
+        }
+
+        public void BasicClick()
+        {
+            string exampleProgram = ExamplePrograms.GetTextBasicExampleProgram();
+
+            _programRichTextBox.Text = File.ReadAllText(exampleProgram);
+            string fileAsText = _programRichTextBox.Text;
+        }
+
+        public void AdvancedClick()
+        {
+            string exampleProgram = ExamplePrograms.GetTextAdvancedExampleProgram();
+
+            _programRichTextBox.Text = File.ReadAllText(exampleProgram);
+            string fileAsText = _programRichTextBox.Text;
+        }
+
+        public void ExpertClick()
+        {
+            string exampleProgram = ExamplePrograms.GetTextExpertExampleProgram();
+
+            _programRichTextBox.Text = File.ReadAllText(exampleProgram);
+            string fileAsText = _programRichTextBox.Text;
+        }
+
+
+        private CodeProgram TextToCodeProgram()
+        {
+            FileInfo file = new FileInfo(@"ProcessingFile\ProcessingFile.txt");
+            string fullFileName = file.FullName;
+
+            _programRichTextBox.SaveFile(fullFileName, RichTextBoxStreamType.PlainText);
+
+            CodeProgram codeProgram = null;
+            IProgramParser parser = new TxtProgramParser();
+            IProgramImporter importer = new TxtProgramImporter(parser);
+            codeProgram = importer.Import(fullFileName);
+            return codeProgram;
+        }
+
+        private int FindGridDimension(Character character)
+        {
+            int gridDimension = 0;
+
+            foreach ((int x, int y) coordinate in character.allPositions)
+            {
+                if (coordinate.x > gridDimension)
+                {
+                    gridDimension = coordinate.x;
+                }
+                if (coordinate.y > gridDimension)
+                {
+                    gridDimension = coordinate.y;
+                }
+            }
+            return gridDimension;
+        }
+
+        private void ColorPad(object sender, PaintEventArgs e)
+        {
+            CodeProgram codeProgram = TextToCodeProgram();
+            Character character = new Character();
+            Trace trace = new Trace();
+            codeProgram.Execute(character, trace);
+
+            if (codeProgram.Commands.Count == 0)
+            {
+                return;
+            }
+
+            float gridDimension = FindGridDimension(character) + 1;
+            float panelMargin = 10;
+
+            Pen penGrid = new Pen(Color.Red, 2);
+            Pen penPath = new Pen(Color.Black, 2);
+            Graphics g = e.Graphics;
+
+            Size size = ((Panel)sender).Size;
+
+            float usableGridSide = size.Width - panelMargin * 2;
+
+            float cellDimension = usableGridSide / gridDimension;
+
+            for (int j = 0; j <= gridDimension; j++)
+            {
+                g.DrawLine(penGrid, panelMargin + j * cellDimension, panelMargin, panelMargin + j * cellDimension, panelMargin + usableGridSide); //vertical
+                g.DrawLine(penGrid, panelMargin, panelMargin + j * cellDimension, panelMargin + usableGridSide, panelMargin + j * cellDimension); //horizontal
+            }
+
+            for (int k = 0; k < character.allPositions.Count - 1; k++)
+            {
+                (int x, int y) end;
+                (int x, int y) start = character.allPositions[k];
+
+                if (k == character.allPositions.Count - 1)
+                {
+                    end = start;
+                }
+
+                else
+                {
+                    end = character.allPositions[k + 1];
+                }
+
+                g.DrawLine(penPath, panelMargin + start.x * cellDimension + (cellDimension / 2), panelMargin + start.y * cellDimension + (cellDimension / 2), panelMargin + end.x * cellDimension + (cellDimension / 2), panelMargin + end.y * cellDimension + (cellDimension / 2));
+            }
+        }
+
+        private void _programPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MSOopdracht2Test/ExceptionTests.cs
@@ -0,0 +1,30 @@
+﻿using MSOopdracht2;
+using MSOopdracht2.Exceptions;
+
+namespace MSOopdracht2Test
+{
+    public class ExceptionTests
+    {
+        [Fact]
+        public void MoveForwardBlockedMoveException()
+        {
+            //Initialize the test objects
+            char[,] mockGrid = { {' '}, {'+'} };
+            Grid grid = new Grid(mockGrid);
+            Character character = new Character(grid);
+
+            Assert.Throws<BlockedMoveException>(() => character.MoveForward(1));
+        }
+
+        [Fact]
+        public void MoveForwardOutOfBoundsException()
+        {
+            //Initialize the test objects
+            char[,] mockGrid = { { ' ' } };
+            Grid grid = new Grid(mockGrid);
+            Character character = new Character(grid);
+
+            Assert.Throws<OutOfBoundsException>(() => character.MoveForward(1));
+        }
+    }
+}
